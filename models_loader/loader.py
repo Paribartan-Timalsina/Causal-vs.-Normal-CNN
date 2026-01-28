458dd0aec33bf9be9ed0dc5ce4a29efa322c2dd3
Paribartan Timalsina <paribartan.timalsina@docsumo.com>
2026-01-28 23:59:31 +0545

    fix: fixed the model path error

--- models_loader/loader.py
+++ models_loader/loader.py
@@ -6,15 +6,15 @@ from .irm import IRM_CNN
 def load_model(model_type, device):
     if model_type == "Simple CNN":
         model = SimpleCNN()
-        model.load_state_dict(torch.load("simple_cnn.pth", map_location=device))
+        model.load_state_dict(torch.load("models/simple_cnn.pth", map_location=device))
 
     elif model_type == "ResNet-18":
         model = get_resnet_model()
-        model.load_state_dict(torch.load("Resnet18.pth", map_location=device))
+        model.load_state_dict(torch.load("models/Resnet18.pth", map_location=device))
 
     else:
         model = IRM_CNN()
-        model.load_state_dict(torch.load("IRM_cnn.pth", map_location=device))
+        model.load_state_dict(torch.load("models/IRM_cnn.pth", map_location=device))
 
     model.to(device)
     model.eval()
